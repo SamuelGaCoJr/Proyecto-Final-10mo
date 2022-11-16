2e5077226a3d35aa27b0e3357c66c8603b6a5954
Samuel <samuelgarciacontreras1@gmail.com>
2022-11-16 14:27:36 -0600

    Carrusel de imagenes responsive

--- index.php
+++ index.php
@@ -5,12 +5,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/index.css">
-    <title>Inicio</title>
+    <title>Inicio SMART-X</title>
 </head>
 <body>
 <!-- esto es el header -->
     <?php
         include "./php/header.php"
     ?>
+
+    <div class="carrusel-img">
+        <ul>
+            <li><img src="./img/productos/carrusel1.png" alt=""></li>
+            <li><img src="./img/productos/carrusel2.png" alt=""></li>
+            <li><img src="./img/productos/carrusel3.png" alt=""></li>
+            <li><img src="./img/productos/carrusel4.png" alt=""></li>
+        </ul>
+    </div>
 </body>
 </html>
