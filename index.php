d2b93c7dbaf78509b0f72f8e28a900f4e1b060b8
Samuel <samuelgarciacontreras1@gmail.com>
2022-11-22 14:35:39 -0600

    Inclusion y footer

--- index.php
+++ index.php
@@ -47,22 +47,36 @@
             <h2 class="titulo-beneficios">Instalación</h2>
         </div>
     </section>
+    <!-- Inclusion -->
+    <section class="inclusion">
+        <div class="todo-inclusion">
+            <h2 class="titulo-inclusion">Inclusión</h2>
+            <label for="" class="texto-inclusion">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum dolorum iste sunt quia id doloribus porro earum incidunt modi unde tempore, deserunt quibusdam mollitia pariatur aut quis fuga doloremque dolore.</label><br>
+            <img src="/img/pagina/inclusion.png" alt="" class="img-inclusion">
+        </div>
+    </section>
     <section class="misyvis">
-    <div class="mision">
-        <h2 class="titulo-mision">Misión</h2>
-        <label for="" class="texto-mision">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed earum, laborum temporibus enim, odio, velit doloribus eaque rem odit quisquam hic at laudantium repudiandae voluptate explicabo quia. Doloribus, quibusdam corrupti! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore necessitatibus nam modi suscipit alias eius beatae impedit deleniti cupiditate dolorem? Vel eius consequatur quibusdam? Expedita tempore odio natus sapiente saepe? <br>
-            <img src="/img/pagina/mision.png" alt="" class="img-mision">
-        </label>
-    </div>
-    <div class="vision">
-        <h2 class="titulo-vision">Visión</h2>
-        <label for="" class="texto-vision">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti rem eius, quibusdam eum alias, facere, laboriosam officia libero adipisci illum quasi. Tenetur, laboriosam cupiditate aliquid deleniti vel quae possimus rerum. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi amet placeat officia eius officiis nulla odit vel! Accusantium, quaerat error eaque repudiandae, molestiae sint nesciunt necessitatibus beatae voluptatum ut ea. <br>
-            <img src="/img/pagina/vision.png" alt="" class="img-vision">
-        </label>
-    </div>
-</section>
+        <div class="mision">
+            <h2 class="titulo-mision">Misión</h2>
+            <label for="" class="texto-mision">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed earum, laborum temporibus enim, odio, velit doloribus eaque rem odit quisquam hic at laudantium repudiandae voluptate explicabo quia. Doloribus, quibusdam corrupti! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore necessitatibus nam modi suscipit alias eius beatae impedit deleniti cupiditate dolorem? Vel eius consequatur quibusdam? Expedita tempore odio natus sapiente saepe? <br>
+                <img src="/img/pagina/mision.png" alt="" class="img-mision">
+            </label>
+        </div>
+        <div class="vision">
+            <h2 class="titulo-vision">Visión</h2>
+            <label for="" class="texto-vision">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti rem eius, quibusdam eum alias, facere, laboriosam officia libero adipisci illum quasi. Tenetur, laboriosam cupiditate aliquid deleniti vel quae possimus rerum. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi amet placeat officia eius officiis nulla odit vel! Accusantium, quaerat error eaque repudiandae, molestiae sint nesciunt necessitatibus beatae voluptatum ut ea. <br>
+                <img src="/img/pagina/vision.png" alt="" class="img-vision">
+            </label>
+        </div>
+    </section>
+    <footer>
+        <?php
+            include "./php/footer.php"
+        ?>
+    </footer>
 </main>
+
 </body>
 </html>
---
+++ php/footer.php
@@ -0,0 +1,13 @@
+<html lang="en">
+    <link rel="stylesheet" href="/css/footer.css">
+    <footer>
+        <nav class="footer">
+            <ul class="lista-footer">
+                <li><a href="#">Aviso de privacidad</a></li>
+                <li><a href="#">Contacto</a></li>
+                <li><a href="#">Cositas</a></li>
+            </ul>
+            <p class="copyright">Todos los derechos reservados 2022 ® SMART-X</p>
+        </nav>
+    </footer>
+</html>
